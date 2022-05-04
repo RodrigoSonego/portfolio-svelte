c0fbf0a3c3fbddddaa92532117c3212f51666402
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-05-04 17:17:37 -0300

    feature: navbar prototype

---
+++ src/lib/Navbar.svelte
@@ -0,0 +1,21 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<nav class="sticky-top navbar navbar-expand-sm navbar-dark bg-dark text-light">
+    <div class="container-fluid">
+        <span class="navbar-brand mb-0 h1"> Rodrigo Sonego </span>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;"></ul>
+            <div class="navbar-nav d-flex justify-content-lg-end me-2">
+                <a class="nav-link active" aria-current="page" href="#">Sobre</a>
+                <a class="nav-link active" href="#">Portfolio</a>
+                <a class="nav-link active" href="#">Contato</a>
+            </div>
+        </div>
+    </div>
+</nav>
+
+<style>
+    .float-right {
+        float: right;
+    }
+</style>
