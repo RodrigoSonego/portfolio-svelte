98985100c015790bc4a0d36b6732eb0836bf8ac5
Rodrigo Sonego <rodrigoasonego@gmail.com>
2022-05-06 11:12:35 -0300

    feature: 'about' section

--- src/lib/Profile.svelte
+++ src/lib/Profile.svelte
@@ -10,56 +10,75 @@
     const mailLink = 'rodrigoasonego@gmail.com'
     const linkedinLink = 'https://www.linkedin.com/in/rodrigo-sônego-2a9aa4211/'
 
-    const logoSize = 35
+    const logoSize = 30
 </script>
 
 <head>
     <script src="https://kit.fontawesome.com/f006e1eac3.js" crossorigin="anonymous"></script>
 </head>
 
-<div class="bg-dark center-block">
-    <img src="https://pbs.twimg.com/profile_images/1293925081542995971/s2la3KS9_400x400.png" class="rounded-circle mx-auto img-responsive d-block profile-image" alt="..."
-    width="200px" height="200px">
-    <!-- <img src={githubLogo} class="rounded-circle mx-auto img-responsive d-block" alt="..."
-    width="250px" height="250px"> -->
-
-    <div class="container">
-        <div class="row">
-            <h1 class="text-light flexbox text-center title">
-                Rodrigo Sônego
-            </h1>
+<div class="bg-dark center-block header">
+    <div class="align-self-center container-fluid">
+        <div class="flex-row">
+            <img src="https://pbs.twimg.com/profile_images/1293925081542995971/s2la3KS9_400x400.png" class="rounded-circle mx-auto img-responsive d-block profile-image" alt="..."
+            width="200px" height="200px">
         </div>
 
+        <div class="container">
+            <div class="row">
+                <h1 class="text-light flexbox text-center title">
+                    Rodrigo Sônego
+                </h1>
+            </div>
 
-        <div class="row">
-            <div class="center icon-row">
-                <div class="col-lg-4 col-md-4 col-xs-4-thumb align-center">
-                    <a class="thumbnail" href={githubLink} target="_blank">
-                        <img src={githubLogo} alt="Github" class="img-responsive"
-                        width="{logoSize}px" height="{logoSize}px">
-                    </a>
-                </div>
-        
-                <div class="col-lg-4 col-md-4 col-xs-4-thumb align-center">
-                    <a class="thumbnail" href="mailto:{mailLink}" target="_blank">
-                        <img src={gmailLogo} alt="Email" class="img-responsive"
-                        width="{logoSize}px" height="{logoSize}px">
-                    </a>
-                </div>
-                <div class="col-lg-4 col-md-4 col-xs-4-thumb align-center">
-                    <a class="thumbnail" href={linkedinLink} target="_blank">
-                        <img src={linkedinLogo} alt="Linkedin" class="img-responsive"
-                        width="{logoSize}px" height="{logoSize}pxpx">
-                    </a>
-                </div>
+            <div class="row">
+                <div class="center icon-row">
+                    <div class="col-lg-4 col-md-4 col-xs-4-thumb align-center">
+                        <a class="thumbnail" href={githubLink} target="_blank">
+                            <img src={githubLogo} alt="Github" class="img-responsive"
+                            width="{logoSize}px" height="{logoSize}px">
+                        </a>
+                    </div>
+            
+                    <div class="col-lg-4 col-md-4 col-xs-4-thumb align-center">
+                        <a class="thumbnail" href="mailto:{mailLink}" target="_blank">
+                            <img src={gmailLogo} alt="Email" class="img-responsive"
+                            width="{logoSize}px" height="{logoSize}px">
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-xs-4-thumb align-center">
+                        <a class="thumbnail" href={linkedinLink} target="_blank">
+                            <img src={linkedinLogo} alt="Linkedin" class="img-responsive"
+                            width="{logoSize}px" height="{logoSize}pxpx">
+                        </a>
+                    </div>
 
-            <!-- <div class="col align-center"> <img style="width: 25%; height: 25%" src="https://image.ibb.co/ckA7ka/mr_marbles.png" alt="Mister Marbles head tilt"> </div>
-            <div class="col align-center"> <img style="width: 25%; height: 25%" src="https://image.ibb.co/bTMKyv/kermit.png" alt="Kermit the Dog"> </div>
-            <div class="col align-center"> <img style="width: 25%; height: 25%" src="https://image.ibb.co/ki8tQa/peach.png" alt="Peach posing magestically"> </div> -->
+                <!-- <div class="col align-center"> <img style="width: 25%; height: 25%" src="https://image.ibb.co/ckA7ka/mr_marbles.png" alt="Mister Marbles head tilt"> </div>
+                <div class="col align-center"> <img style="width: 25%; height: 25%" src="https://image.ibb.co/bTMKyv/kermit.png" alt="Kermit the Dog"> </div>
+                <div class="col align-center"> <img style="width: 25%; height: 25%" src="https://image.ibb.co/ki8tQa/peach.png" alt="Peach posing magestically"> </div> -->
+                </div>
             </div>
         </div>
+    </div> 
+</div>
+
+<div class="bg-light about-container">
+    <div class="about-info-container">
+       <p> 
+        <h1 class="flex-block text-left" > 
+          <strong>
+            Olá! 
+          </strong> 
+        </h1>
+       <p class="flex-block text-left"> 
+            Meu nome é Rodrigo Sônego.
+            <br>
+            Sou acadêmico do curso de <strong>Ciência da computação</strong>, com experiência em
+            <strong>desenvolvimento de jogos, sistemas</strong> e <strong>aplicações de reliadade aumentada</strong>.
+            <br>
+            Sinta-se à vontade para entrar em contato ou conferir meu trabalho abaixo
+       </p>
     </div>
-    
 </div>
 
 <style>
@@ -74,13 +93,13 @@
     /*CENTER DIV*/
     .center {
         margin: 0 auto;
-        width: 30%;
+        width: 25%;
         display: flex;
     }
 
     .icon-row {
         margin-top: 10px;
-        margin-bottom: 10px;
+        margin-bottom: 20px;
     }
 
     .title {
@@ -88,5 +107,25 @@
         margin: 10px;
         font-family: "Roboto", "Helvetica", "Arial", sans-serif;;
         font-weight: 400;
+    }
+
+    /* @media (min-width: 600px) */
+    .about-container {
+        padding-left: 24px;
+        padding-right: 24px;    
+        min-width: 600px;
+    }
+
+    .about-info-container {
+        padding: 1rem 0;
+        font-size: 1.5rem;
+        font-weight: 300;
+
+        margin-right: 10%;
+        margin-left: 10%;
+    }
+
+    .header {
+        min-height: 500px;
     }
 </style>
